cf4ba8ab81898e1ad988012584965f5b8b4850fb
ader proyecto <proyectoabo79@gmail.com>
2025-08-09 23:15:31 -0500

    continuacion

---
+++ public/scripts/loginprueba01.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+const container = document.querySelector('.container');
+
+
+btn.addEventListener('click', () =>{
+   container.classList.toggle('toggle');
+
+});
